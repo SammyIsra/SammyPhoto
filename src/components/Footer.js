4f91bc03c6a9da36386895caa8d4dabb06774fb1
Sammy Israwi <sammy.israwi@gmail.com>
2016-11-20 14:29:19 -0500

    Minor font changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,8 @@ class Footer extends Component {
                 <div className="container">
                     <div className="row twelve columns">
                         <ul className="social">
-                            <li className="facebook"><a href="https://www.facebook.com/sammy.israwi"><i className="fa fa-facebook-square fa-lg"></i></a></li>
+                            <li className="github"><a href="https://github.com/SammyIsra"><i className="fa fa-github-square fa-lg"></i></a></li>
+                            {/*<li className="facebook"><a href="https://www.facebook.com/sammy.israwi"><i className="fa fa-facebook-square fa-lg"></i></a></li> */}
                             <li className="instagram"><a href="https://www.instagram.com/sammyisra"><i className="fa fa-instagram fa-lg"></i></a></li>
                             <li className="flickr"><a href="https://www.flickr.com/sammy_iy"><i className="fa fa-flickr fa-lg"></i></a></li>
                             <li className="linkedin"><a href="https://www.linkedin.com/in/sammyisrawi"><i className="fa fa-linkedin-square fa-lg"></i></a></li>
