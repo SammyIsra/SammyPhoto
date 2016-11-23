884122a4aba13f276e051263237758bf69928ac2
Sammy Israwi <sammy.israwi@gmail.com>
2016-11-23 01:18:04 -0500

    Changed the bio and styling

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -10,7 +10,12 @@ class Bio extends Component {
                     <div className="twelve columns">
                         <h2 className="bio-title">About Sammy Israwi</h2>
                         <p className="bio">
-                            Sammy is a Computer Science student at the University of Central Florida. In his free time, Sammy likes to play video games, watch movies, and most specially take pictures. His favorite subject is people, but Sammy also has a collection of landscapes, insects, street art and spontaneous shots.
+                            Sammy is a Computer Science student at the University
+                            of Central Florida. He enjoys videogames, movies, and attending art galleries.
+                            However, his biggest passion is photography.
+                            His main subjects are street art and spontaneity,
+                            but what he loves the most is taking portraits of people.<br />
+                            Feel free to reach him via social media!
                         </p>
                     </div>
                 </div>
