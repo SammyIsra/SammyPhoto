63b1ab16179075795f4445985b43f8475f9a84fc
Sammy Israwi <sammy.israwi@gmail.com>
2016-12-29 17:56:39 -0500

    Lightbox working and pretty now

--- src/components/Lightbox.js
+++ src/components/Lightbox.js
@@ -5,19 +5,22 @@ import './Lightbox.css';
 const Lightbox = (props) => {
 
     const photo = props.photo;
+    const srcLink = `https://www.flickr.com/photos/${photo.owner}/${photo.id}/`
 
     return (
         <div id="myModal" className="modal">
             <span className="close cursor" onClick={() => props.onClose()}>&times;</span>
+
             <div className="modal-content">
+                <a href={srcLink} target="_blank">
+                    <img
+                        src={photo.url_l}
+                        alt={photo.title}  />
 
-                <img
-                    src={photo.url_l}
-                    alt={photo.title}  />
-
-                <div className="caption-container">
-                    <p id="title">{photo.title}</p>
-                </div>
+                    <div className="caption-container">
+                        <p id="title">{photo.title}</p>
+                    </div>
+                </a>
 
             </div>
         </div>
